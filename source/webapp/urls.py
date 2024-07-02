e0c3511a3ec152770551762698683183433aee87
Vitya Endless <dvitya031190@gmail.com>
2024-07-02 21:14:06 +0600

    add base classes and ui

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
+from .views import IndexView, AddView, SubtractView, MultiplyView, DivideView
+
 
 app_name = 'webapp'
 
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('add/', AddView.as_view(), name='add'),
+    path('subtract/', SubtractView.as_view(), name='subtract'),
+    path('multiply/', MultiplyView.as_view(), name='multiply'),
+    path('divide/', DivideView.as_view(), name='divide'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class AddView(View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse([{'id': 1, 'name': 'test'}, {'id': 2, 'name': 'test2'}], safe=False)
+
+
+class SubtractView(View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse([{'id': 1, 'name': 'test'}, {'id': 2, 'name': 'test2'}], safe=False)
+
+
+class MultiplyView(View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse([{'id': 1, 'name': 'test'}, {'id': 2, 'name': 'test2'}], safe=False)
+
+
+class DivideView(View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse([{'id': 1, 'name': 'test'}, {'id': 2, 'name': 'test2'}], safe=False)
